38ed8e9dae5f082f32000cae408ba18d1a77c403
Andrew Chin <achin83@gmail.com>
2018-08-29 21:46:11 -0700

    Final Commit
    This was a fun hw assignment.

--- PyBank/Main/main.py
+++ PyBank/Main/main.py
@@ -1,14 +1,67 @@
-#importing necessary OS and CSV libraries to read CSV file
+#PYBANK
+#---------------------------------------------------------------------
+
+#importing necessary OS and CSV libraries
 import os
 import csv
-#locate CSV file path
-pyBankFile = os.path.join('..', 'Resources', 'budget_data.csv')
-#open CSV file
-with open(pyBankFile, newline='') as csvfile:
 
-    #declare filename with specified delimiter as comma
+#prepare empty lists to store CSV data
+list_profitloss = []
+list_months = []
+list_minmax = []
+
+#locate CSV file path
+budget_data = os.path.join('..', 'Resources', 'budget_data.csv')
+#open CSV file
+with open(budget_data, newline='') as csvfile:
+
+    #declare filename with specified delimiter as comma, using reader function
     csvreader = csv.reader(csvfile, delimiter = ',')
-    #ignore the header
+    #ignore the header to avoid counting this row as month
     next(csvfile)
+
     for row in csvreader:
-        print(row)
+        #append only profit/loss to list for row count and average between month analysis
+        list_months.append(str(row[0]))
+        list_profitloss.append(int(row[1]))
+
+#declare variables from populated lists
+total_revenue = sum(list_profitloss)
+total_months = len(list_months)
+max_val = max(list_profitloss)
+min_val = min(list_profitloss)
+#zipped the profit/loss and month list with profit/loss first, so I can grab the row with either min/max value
+list_minmax = list(zip(list_profitloss, list_months))
+
+#use list comprehension to structure and populate a newly calculated net change list
+#net change = difference between the current record - previous record
+#use zip to compare: P/L list at index[0] with same P/L list at index[1] 
+#subtract y-x for each record in list to get net change. 
+# No value will be calculated for 10-Jan
+net_change = [y - x for x, y in zip(list_profitloss, list_profitloss[1:])]
+
+
+#declare a function to calculate straight average
+def straight_average(num_list):
+    total = 0.0
+    length = len(num_list)
+    for num in num_list:
+        total += float(num)
+    return(total / length)
+
+#output results to console
+print(f'\nFinancial Analysis\n--------------------------------')
+print(f'Total Months:    {total_months}\nTotal Revenue:   ${total_revenue}')
+print(f'Average Change:  ${round(straight_average(net_change),2)}')
+print(f'Greatest Increase in Profits: {max(list_minmax)[1]}: {max(list_minmax)[0]}')
+print(f'Greatest Decrease in Profits: {min(list_minmax)[1]}: {min(list_minmax)[0]}')
+
+#output results to CSV (in same path as main.py)
+output_path = os.path.join("Financial_Results.csv")
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Total Months:', total_months])
+    csvwriter.writerow(['Total Revenue:', total_revenue])
+    csvwriter.writerow(['Average Change:', round(straight_average(net_change),2)])
+    csvwriter.writerow(['Greatest Increase in Profits:', max(list_minmax)[1], max(list_minmax)[0]])
+    csvwriter.writerow(['Greatest Decrease in Profits:', min(list_minmax)[1], min(list_minmax)[0]])
---
+++ PyPoll/Main/main.py
@@ -0,0 +1,62 @@
+#PYPOLL
+#---------------------------------------------------------------------
+
+#importing necessary OS and CSV libraries
+import os
+import csv
+
+list_voter = []
+vote_khan = []
+vote_correy = []
+vote_li = []
+vote_otooley = []
+
+#locate CSV file path
+election_data = os.path.join('..', 'Resources', 'election_data.csv')
+#open CSV file
+with open(election_data, newline='') as csvfile:
+
+    #declare filename with specified delimiter as comma, using reader function
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    #ignore the header to avoid counting this row as month
+    next(csvfile)
+
+    for row in csvreader:
+        #append only profit/loss to list for row count and average between month analysis
+        list_voter.append(row)
+
+#loop through and build separate lists per candidate
+for i in list_voter:
+    if i[2] == "Khan":
+        vote_khan.append(i[2])
+    elif i[2] == "Correy":
+        vote_correy.append(i[2])
+    elif i[2] == "Li":
+        vote_li.append(i[2])
+    elif i[2] == "O'Tooley":
+        vote_otooley.append(i[2])
+
+#build a dictionary for built lists to derive winner
+dict_votes = {"Khan": len(vote_khan), "Correy": len(vote_correy), "Li": len(vote_li), "O'Tooley": len(vote_otooley)}
+winner = max(zip(dict_votes.values(), dict_votes.keys()))
+
+#output results
+print(f'\nElection Results\n-------------------------')
+print(f'Total Votes: {len(list_voter)}\n-------------------------')
+print(f'Khan: {round((len(vote_khan) / len(list_voter) * 100), 2)}% ({len(vote_khan)})')
+print(f'Correy: {round((len(vote_correy) / len(list_voter) * 100), 2)}% ({len(vote_correy)})')
+print(f'Li: {round(len(vote_li) / len(list_voter) * 100, 2)}% ({len(vote_li)})')
+print(f'O\'Tooley: {round(len(vote_otooley) / len(list_voter) * 100, 2)}% ({len(vote_otooley)})')
+print(f'------------------------')
+print(f'Winner: {winner[1]}\n------------------------')
+
+#output results to CSV (in same path as main.py)
+output_path = os.path.join("Election_Data.csv")
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Election Results:'])
+    csvwriter.writerow(['Khan:', len(vote_khan) / len(list_voter), len(vote_khan)])
+    csvwriter.writerow(['Correy:', len(vote_correy) / len(list_voter), len(vote_correy)])
+    csvwriter.writerow(['Li:', len(vote_li) / len(list_voter), len(vote_li)])
+    csvwriter.writerow(['O\'Tooley:', len(vote_otooley) / len(list_voter), len(vote_otooley)])
+    csvwriter.writerow(['Winner:', winner[1]])
